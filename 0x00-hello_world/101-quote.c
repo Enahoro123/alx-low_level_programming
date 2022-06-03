69ef76275bb3f53f9870e2eedccbe582bd1d5210
Enahoro123 <uvieeu@gmailcom>
2022-06-02 18:44:41 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str0[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str1, 59);
+	write(2, str0, 59);
 	return (1);
 }
