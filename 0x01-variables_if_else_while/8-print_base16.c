4a54249169e51bf8aa4b8950bccc99ff6f02f933
Enahoro123 <uvieeu@gmailcom>
2022-06-03 18:13:06 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Print all numbers of base16 in lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+	char al;
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (al = 'a'; al < 'f'; al++)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+
+	return (0);
+}
