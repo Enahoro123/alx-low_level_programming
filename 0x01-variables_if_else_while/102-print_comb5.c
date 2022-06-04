15d6d6ea5063a7a2fdefee32f727d3713fcdad61
Enahoro123 <uvieeu@gmailcom>
2022-06-03 19:12:02 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int e, f;
+
+	for (e = 0; e < 100; e++)
+	{
+		for (f = 0; f < 100; f++)
+		{
+			if (e < j)
+			{
+				putchar((e / 10) + 48);
+				putchar((e % 10) + 48);
+				putchar(' ');
+				putchar((f / 10) + 48);
+				putchar((f % 10) + 48);
+				if (e != 98 || f != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
