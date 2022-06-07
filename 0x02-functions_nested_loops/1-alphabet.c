ea9978bdfe3db8fef8542b49c998dd3c1129c1c0
Enahoro123 <uvieeu@gmailcom>
2022-06-07 16:58:19 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_al - Print alphabet in lowercase using only `_putchar`
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
  * Return: 0
  */
-void print_al(void)
+void print_alphabet(void)
 {
 	char al = 'a';
 
