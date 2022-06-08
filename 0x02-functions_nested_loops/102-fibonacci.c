fc7ba06e214a4551f0efc43f12bb04de0f4aac0a
Enahoro123 <uvieeu@gmailcom>
2022-06-07 19:32:54 -0700

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
+ * Numbers must have coma and space separated.
+ * Return: 0
+ */
+int main(void)
+{
+	int count = 2;
+	long int a = 1, b = 2;
+	long int c;
+
+	printf("%lu, ", a);
+	while (count <= 50)
+	{
+		if (count == 50)
+		{
+			printf("%lu\n", b);
+		}
+		else
+		{
+			printf("%lu, ", c);
+		}
+
+		c = b;
+		b += a;
+		a = c;
+		count++;
+	}
+
+	return (0);
+}
