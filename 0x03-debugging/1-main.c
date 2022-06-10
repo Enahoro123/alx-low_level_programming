505f3ec9e8c231b8f347e6b16555f9b04d046a6a
Enahoro123 <uvieeu@gmailcom>
2022-06-09 18:03:34 -0700

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,10 +15,10 @@ int main(void)
 
         while (i < 10)
         {
-                putchar(i); /* don’t delete it! */
+                putchar(i);
         }
 
         printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+        return (0); /* don’t delete it! */
 }
