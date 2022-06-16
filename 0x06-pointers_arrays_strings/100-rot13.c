c784a2b50a98f7a2da3058bb239520cf4bc7b1d5
Enahoro123 <uvieeu@gmailcom>
2022-06-16 14:27:16 -0700

    7

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,24 +1,27 @@
 #include "main.h"
 
 /**
- * rot13 - rotate characters 13 places in the alphabet
+ * rot13 - encodes a string using rot13
  * @s: string
- * Return: string `s` rotated
+ * Return: the pointer to dest
  */
 
 char *rot13(char *s)
 {
-	int i;
-	char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char storel[] = "nopqrstuvwxyzabcdefghijklm";
-
-	for (i = 0; s[i] != '\0'; i++)
+	int count = 0, i;
+	char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	
+	while (*(s + count) != '\0')
 	{
-		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
+		for (i = 0; i < 52; i++)
 		{
-			s[i] = (s[i] - 65 > 25) ?
-				storel[s[i] - 97] : storeh[s[i] - 65];
+			if (*(s + count) == alph[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}	
 		}
+		count++;
 	}
-	return (s);
 }
